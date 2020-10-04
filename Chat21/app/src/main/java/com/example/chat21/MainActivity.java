6c2011987aa1200b21731d55ef4e2d37fed33aa8
Vedant <vedant@coffeedost.com>
2020-10-04 18:52:25 +0530

    implemented chat21 library

---
+++ Chat21/app/src/main/java/com/example/chat21/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.chat21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.chat21.android.core.ChatManager;
+import org.chat21.android.core.authentication.ChatAuthentication;
+import org.chat21.android.core.contacts.listeners.OnContactCreatedCallback;
+import org.chat21.android.core.exception.ChatRuntimeException;
+import org.chat21.android.core.users.models.IChatUser;
+import org.chat21.android.ui.ChatUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        ChatManager.startWithEmailAndPassword(this, getString(R.string.google_app_id),
+  "vedant@gmail.com","admin9799", new ChatAuthentication.OnChatLoginCallback() {
+            @Override
+            public void onChatLoginSuccess(IChatUser currentUser) {
+                ChatManager.getInstance().createContactFor(currentUser.getId(), currentUser.getEmail(),
+                        "vedant", "patil", new OnContactCreatedCallback() {
+                    @Override
+                    public void onContactCreatedSuccess(ChatRuntimeException exception) {
+                        if (exception == null) {
+                            ChatUI.getInstance().openConversationsListActivity();
+                        } else {
+                            // TODO: handle the exception
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onChatLoginError(Exception e) {
+                // TODO: 22/02/18
+            }
+        });
+    }
+}
